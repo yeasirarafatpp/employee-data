783cf84015f0729192c40c20db5da770da1a9130
yeasirarafatpp <yeasira79@gmail.com>
2021-09-25 23:44:57 +0600

    Create Fake Data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import Body from './components/Body/Body';
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <div>
+      <Header></Header>
       <Body></Body>
     </div>
   );
---
+++ src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+    return (
+        <div>
+            <div>
+
+            </div>
+        </div>
+    );
+};
+
+export default Header;
